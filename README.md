b3b9bd265e86932bbfc6e22596e826c234fdec60
Nikola Tomic <ntomic@3d4pro.fr>
2019-05-15 15:00:22 +0200

    generated pdf with all qr codes

--- README.md
+++ README.md
@@ -2,4 +2,7 @@ install dependency:
 npm install -g qrcode
 
 to generate qr codes launch:
-python generate\_qr\_code.py places.json qr\_codes
+python generate\_qr\_codes.py places.json qr\_codes
+
+to generate pdf with all qr codes:
+python generate_qr_code_pdfs.py qr_codes.pdf qr_codes *nb_columns* *nb_rows*
---
+++ generate_qr_code_pdfs.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import cm
+
+
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+
+def main(pdf_file, qr_code_dir, nb_columns, nb_rows):
+	c = canvas.Canvas(pdf_file, bottomup=False)
+	width = c._pagesize[0] / nb_columns
+	image_height = width
+	text_height = 0.2 * cm
+	character_width = 0.23 * cm
+	height = image_height + text_height
+	images = sorted(os.listdir(qr_code_dir))
+	for page in chunker(images, nb_columns * nb_rows):
+		for j, row in enumerate(chunker(page, nb_columns)):
+			for i, image in enumerate(row):
+				c.drawImage("{}/{}".format(qr_code_dir, image), width * i, height * j, width, image_height)
+				image_name = os.path.splitext(image)[0]
+				c.drawString(width * i + width / 2 - (len(image_name) * character_width) / 2, height * j + image_height, image_name)
+		c.showPage()
+	c.save()
+
+
+main(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
