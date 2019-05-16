dc37ce18768f99e6eb2038ccf8778c2047c3b935
Nikola Tomic <ntomic@3d4pro.fr>
2019-05-16 15:23:37 +0200

    centered qr codes

--- generate_qr_code_pdf.py
+++ generate_qr_code_pdf.py
@@ -11,16 +11,17 @@ def chunker(seq, size):
 def main(pdf_file, qr_code_dir, nb_columns, nb_rows):
 	c = canvas.Canvas(pdf_file)
 	width = c._pagesize[0] / nb_columns
-	page_height = c._pagesize[1]
-	image_height = width
 	text_height = 0.2 * cm
+	height = c._pagesize[1] / nb_rows
+	page_height = c._pagesize[1]
+	image_width = min([width, height - text_height])
+	image_height = image_width
 	character_width = 0.23 * cm
-	height = image_height + text_height
 	images = sorted(os.listdir(qr_code_dir))
 	for page in chunker(images, nb_columns * nb_rows):
 		for j, row in enumerate(chunker(page, nb_columns)):
 			for i, image in enumerate(row):
-				c.drawImage("{}/{}".format(qr_code_dir, image), width * i, page_height - height * j - image_height, width, image_height)
+				c.drawImage("{}/{}".format(qr_code_dir, image), width * i + width / 2 - image_width / 2, page_height - height * j - image_height, image_width, image_height)
 				image_name = os.path.splitext(image)[0]
 				c.drawString(width * i + width / 2 - (len(image_name) * character_width) / 2, page_height - (height * j + image_height), image_name)
 		c.showPage()
