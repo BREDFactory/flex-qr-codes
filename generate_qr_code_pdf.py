688f304c7f290554f8de6606c0b6519e9af2d9d5
Nikola Tomic <ntomic@3d4pro.fr>
2019-05-16 09:16:10 +0200

    turned images upside down

--- generate_qr_code_pdf.py
+++ generate_qr_code_pdf.py
@@ -9,8 +9,9 @@ def chunker(seq, size):
 
 
 def main(pdf_file, qr_code_dir, nb_columns, nb_rows):
-	c = canvas.Canvas(pdf_file, bottomup=False)
+	c = canvas.Canvas(pdf_file)
 	width = c._pagesize[0] / nb_columns
+	page_height = c._pagesize[1]
 	image_height = width
 	text_height = 0.2 * cm
 	character_width = 0.23 * cm
@@ -19,9 +20,9 @@ def main(pdf_file, qr_code_dir, nb_columns, nb_rows):
 	for page in chunker(images, nb_columns * nb_rows):
 		for j, row in enumerate(chunker(page, nb_columns)):
 			for i, image in enumerate(row):
-				c.drawImage("{}/{}".format(qr_code_dir, image), width * i, height * j, width, image_height)
+				c.drawImage("{}/{}".format(qr_code_dir, image), width * i, page_height - height * j - image_height, width, image_height)
 				image_name = os.path.splitext(image)[0]
-				c.drawString(width * i + width / 2 - (len(image_name) * character_width) / 2, height * j + image_height, image_name)
+				c.drawString(width * i + width / 2 - (len(image_name) * character_width) / 2, page_height - (height * j + image_height), image_name)
 		c.showPage()
 	c.save()
 
