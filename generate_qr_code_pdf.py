5d3ad3a111894e4fa28f35b5d7bdf7427931259d
Nikola Tomic <ntomic@3d4pro.fr>
2019-05-16 16:57:03 +0200

    class

--- generate_qr_code_pdf.py
+++ generate_qr_code_pdf.py
@@ -8,24 +8,44 @@ def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
+class Generator:
+	def __init__(self, pdf_file, qr_code_dir, nb_columns, nb_rows):
+		self.pdf_file = pdf_file
+		self.qr_code_dir = qr_code_dir
+		self.nb_columns = nb_columns
+		self.nb_rows = nb_rows
+		self.c = canvas.Canvas(self.pdf_file)
+		self.font_size = 8
+		self.c.setFontSize(self.font_size)
+		self.width = self.c._pagesize[0] / self.nb_columns
+		self.text_height = self.c._fontsize
+		self.height = self.c._pagesize[1] / self.nb_rows
+		self.page_height = self.c._pagesize[1]
+		self.image_width = min([self.width, self.height - self.text_height])
+		self.image_height = self.image_width
+		self.character_width = self.c._fontsize * 0.54
+
+	def generate(self):
+		images = sorted(os.listdir(self.qr_code_dir))
+		for page in chunker(images, self.nb_columns * self.nb_rows):
+			self.c.setFontSize(self.font_size)
+			for j, row in enumerate(chunker(page, self.nb_columns)):
+				for i, image in enumerate(row):
+					self.c.drawImage("{}/{}".format(self.qr_code_dir, image), self.width * i + self.width / 2 - self.image_width / 2, self.page_height - self.height * j - self.image_height, self.image_width, self.image_height)
+					image_name = os.path.splitext(image)[0]
+					self.drawRotatedString(0, self.width * i + self.width / 2 - (len(image_name) * self.character_width) / 2, self.page_height - (self.height * j + self.image_height + self.text_height / 2), image_name)
+			self.c.showPage()
+		self.c.save()
+
+	def drawRotatedString(self, rotation, x, y, text):
+		self.c.rotate(rotation)
+		self.c.drawString(x, y, text)
+		self.c.rotate(360 - rotation)
+
+
 def main(pdf_file, qr_code_dir, nb_columns, nb_rows):
-	c = canvas.Canvas(pdf_file)
-	width = c._pagesize[0] / nb_columns
-	text_height = 0.2 * cm
-	height = c._pagesize[1] / nb_rows
-	page_height = c._pagesize[1]
-	image_width = min([width, height - text_height])
-	image_height = image_width
-	character_width = 0.23 * cm
-	images = sorted(os.listdir(qr_code_dir))
-	for page in chunker(images, nb_columns * nb_rows):
-		for j, row in enumerate(chunker(page, nb_columns)):
-			for i, image in enumerate(row):
-				c.drawImage("{}/{}".format(qr_code_dir, image), width * i + width / 2 - image_width / 2, page_height - height * j - image_height, image_width, image_height)
-				image_name = os.path.splitext(image)[0]
-				c.drawString(width * i + width / 2 - (len(image_name) * character_width) / 2, page_height - (height * j + image_height), image_name)
-		c.showPage()
-	c.save()
+	gen = Generator(pdf_file, qr_code_dir, nb_columns, nb_rows)
+	gen.generate()
 
 
 main(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
