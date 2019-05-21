5a0d8c452ec06e02bbf7b3b2cff8cb50c384cae3
Nikola Tomic <ntomic@3d4pro.fr>
2019-05-21 10:21:59 +0200

    added margins

--- generate_qr_code_pdf.py
+++ generate_qr_code_pdf.py
@@ -10,17 +10,23 @@ def chunker(seq, size):
 
 class Generator:
 	def __init__(self, pdf_file, qr_code_dir, nb_columns, nb_rows):
+		self.left_margin = 0.9 * cm
+		self.right_margin = self.left_margin
+		self.top_margin = self.left_margin
+		self.bottom_margin = self.left_margin
 		self.pdf_file = pdf_file
 		self.qr_code_dir = qr_code_dir
 		self.nb_columns = nb_columns
 		self.nb_rows = nb_rows
 		self.c = canvas.Canvas(self.pdf_file)
+		self.page_x = self.c._pagesize[0] - self.left_margin - self.right_margin
+		self.page_y = self.c._pagesize[1] - self.top_margin - self.bottom_margin
 		self.font_size = 8
 		self.c.setFontSize(self.font_size)
-		self.width = self.c._pagesize[0] / self.nb_columns
+		self.width = self.page_x / self.nb_columns
 		self.text_height = self.c._fontsize
-		self.height = self.c._pagesize[1] / self.nb_rows
-		self.page_height = self.c._pagesize[1]
+		self.height = self.page_y / self.nb_rows
+		self.page_height = self.page_y
 		self.image_width = min([self.width, self.height])
 		self.image_height = self.image_width
 		self.character_width = self.c._fontsize * 0.25
@@ -32,9 +38,9 @@ class Generator:
 			self.c.setFontSize(self.font_size)
 			for j, row in enumerate(chunker(page, self.nb_columns)):
 				for i, image in enumerate(row):
-					self.c.drawImage("{}/{}".format(self.qr_code_dir, image), self.width * i + (self.width / 4 * 3) - self.image_width / 2, self.page_height - (self.height * j + self.image_height), self.image_width, self.image_height)
+					self.c.drawImage("{}/{}".format(self.qr_code_dir, image), self.left_margin + self.width * i + (self.width / 4 * 3) - self.image_width / 2, self.bottom_margin + self.page_height - (self.height * j + self.image_height), self.image_width, self.image_height)
 					image_name = os.path.splitext(image)[0]
-					self.c.drawString(self.width * i + self.width / 4 - (len(image_name) * self.character_width), self.page_height - (self.height * j + self.height / 2), image_name)
+					self.c.drawString(self.left_margin + self.width * i + self.width / 4 - (len(image_name) * self.character_width), self.bottom_margin + self.page_height - (self.height * j + self.height / 2), image_name)
 			self.c.showPage()
 		self.c.save()
 
